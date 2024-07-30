781c98e1c90fb459aefc3c1e0d2501817cae0512
Maksym Volkov <maksym.volkov@zapal.tech>
2024-07-30 16:28:24 +0000

    feat: locales

--- src/app/locales/en.ts
+++ src/app/locales/en.ts
@@ -4,4 +4,32 @@ export default {
     contacts: 'Contacts:',
     messengers: 'Messengers:',
   },
+  search: {
+    table: {
+      originalLastName: 'Last name( original from document)',
+      year: 'The year of information fixation',
+      town: 'Town',
+      address: 'Church attendance or address',
+      populationGroup: 'Population group according to the document',
+      socialStatus: 'Social status / background',
+      docName: 'File name and link',
+    },
+    form: {
+      label: 'Last name',
+      submit: 'Search',
+    },
+    notFound: {
+      title: 'Last name not found',
+      description: 'Check the spelling',
+    },
+    content: {
+      title: 'Start studying your family history',
+      description:
+        'If you are looking for your ancestors in our region or are simply interested in the local history of the ancient city of Chuguiv and nearby villages, then come to us! We will not search for you, we will help you master the wisdom of genealogy and finding information about your ancestors in archival sources, we will tell you about their varieties and features.Discover the fascinating world of family research!',
+      linkText: 'How to start your research?',
+    },
+  },
+  checkDoc: {
+    helpRead: 'Help read',
+  },
 } as const;
