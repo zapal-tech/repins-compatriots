70126565169b1f5b1c553cc6cf313496d9e86137
Maksym Volkov <maksym.volkov@zapal.tech>
2024-08-06 16:43:56 +0000

    feat: shared email senderName, senderEmail, replyToEmail

--- src/shared/email.ts
+++ src/shared/email.ts
@@ -1,4 +1,8 @@
 import { appName } from '@shared';
 
+export const senderName = appName;
+export const senderEmail = 'hello@repins-compatriots.org';
+export const replyToEmail = 'no-reply@repins-compatriots.org';
+
 export const cmsSenderName = `${appName} | CMS`;
 export const cmsSenderEmail = 'hello@repins-compatriots.org';
