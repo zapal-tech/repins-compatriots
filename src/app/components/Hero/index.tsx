ca19bca725f4b5c28fafd923eb340437f07ecff7
Maksym Volkov <maksym.volkov@zapal.tech>
2024-08-07 12:12:49 +0000

    fix: component Hero style

--- src/app/components/Hero/index.tsx
+++ src/app/components/Hero/index.tsx
@@ -3,29 +3,31 @@ import clsx from 'clsx';
 import { HeroType } from '@cms/types';
 import { Page } from '@cms/types/generated-types';
 
-import { Gutter } from '../Gutter';
 import { Media } from '../Media';
 import { RichText } from '../RichText';
 
 export const Hero: React.FC<Page['hero']> = ({ media, richText, type }) => {
   if (type === 'none') return null;
 
+  const HeroTypeLarge = HeroType.Large;
+  const HeroTypeMedium = HeroType.Medium;
+
   return (
     <div className="relative pt-12 xl:pt-20">
       <div
-        className={clsx('relative', {
-          'h-[100vh]': [HeroType.Large === type],
-          'h-max min-h-64': [HeroType.Medium === type],
-          '': [HeroType.Small === type],
-        })}
+        className={clsx(
+          'relative',
+          HeroTypeLarge === type && 'xl:h-[calc(100vh-79px)]',
+          HeroTypeMedium === type && 'min-h-64 xl:h-[calc(100vh-79px)]',
+        )}
       >
         {[HeroType.Large, HeroType.Medium].includes(type as HeroType) && media && typeof media === 'object' ? (
           <Media
             className="size-full overflow-hidden"
             imgClassName={clsx(
               'w-full',
-              type === HeroType.Large ? 'h-full object-cover' : 'min-h-64 object-cover sm:object-contain',
-              type === HeroType.Medium && '',
+              type === HeroType.Large ? 'h-full object-cover' : 'min-h-64 object-cover',
+              type === HeroType.Medium && 'h-full object-cover',
             )}
             resource={media}
             unoptimized
