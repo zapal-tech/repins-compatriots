7da70ff2b4a85e89b024b8941bf4b92ddc6669ff
Maksym Volkov <maksym.volkov@zapal.tech>
2024-08-14 13:24:19 +0000

    fix: components Hero, PageGutter style

--- src/app/components/Hero/index.tsx
+++ src/app/components/Hero/index.tsx
@@ -3,6 +3,7 @@ import clsx from 'clsx';
 import { HeroType } from '@cms/types';
 import { Page } from '@cms/types/generated-types';
 
+import { Gutter } from '../Gutter';
 import { Media } from '../Media';
 import { RichText } from '../RichText';
 
@@ -17,7 +18,7 @@ export const Hero: React.FC<Page['hero']> = ({ media, richText, type }) => {
       <div
         className={clsx(
           'relative',
-          HeroTypeLarge === type && 'xl:h-[calc(100vh-79px)]',
+          HeroTypeLarge === type && 'h-[calc(100vh-47px)] xl:h-[calc(100vh-79px)]',
           HeroTypeMedium === type && 'min-h-[27rem] xl:h-[calc(100vh-79px)]',
         )}
       >
@@ -34,14 +35,27 @@ export const Hero: React.FC<Page['hero']> = ({ media, richText, type }) => {
           />
         ) : null}
 
-        <RichText
-          className={clsx(
-            'flex size-full max-w-full flex-col justify-end px-4 py-6 xl:px-12 xl:py-8',
-            [HeroType.Large, HeroType.Medium].includes(type as HeroType) && 'absolute bottom-0 text-white',
-          )}
-        >
-          {richText}
-        </RichText>
+        {type !== HeroType.Small ? (
+          <RichText
+            className={clsx(
+              'flex size-full max-w-full flex-col justify-end px-4 py-6 xl:px-12 xl:py-8',
+              [HeroType.Large, HeroType.Medium].includes(type as HeroType) && 'absolute bottom-0 text-white',
+            )}
+          >
+            {richText}
+          </RichText>
+        ) : (
+          <Gutter>
+            <RichText
+              className={clsx(
+                'flex size-full max-w-full flex-col justify-end pt-6 xl:pt-8',
+                [HeroType.Large, HeroType.Medium].includes(type as HeroType) && 'absolute bottom-0 text-white',
+              )}
+            >
+              {richText}
+            </RichText>
+          </Gutter>
+        )}
       </div>
     </div>
   );
--- src/app/components/PageGutter.tsx
+++ src/app/components/PageGutter.tsx
@@ -3,5 +3,5 @@ type PageGutterProps = {
 };
 
 export const PageGutter: React.FC<PageGutterProps> = ({ children }) => (
-  <div className="xl:pb-18 pb-10 pt-14 lg:pb-14 lg:pt-20 xl:pt-28">{children}</div>
+  <div className="pb-28 pt-14 lg:pb-32 xl:pb-40 xl:pt-24">{children}</div>
 );
