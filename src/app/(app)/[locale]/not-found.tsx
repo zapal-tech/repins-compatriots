5aecd76382686007dde849f2fa617d5301833805
Maksym Volkov <maksym.volkov@zapal.tech>
2024-08-14 13:22:30 +0000

    feat: not-found localize

--- src/app/(app)/[locale]/not-found.tsx
+++ src/app/(app)/[locale]/not-found.tsx
@@ -1,10 +1,18 @@
 import { MessageScreen, MessageScreenButtonProps } from '@app/components/MessageScreen';
 import { PageGutter } from '@app/components/PageGutter';
 
-const NotFound = async () => (
-  <PageGutter>
-    <MessageScreen title="404" description="Сторінку не знайдено" />
-  </PageGutter>
-);
+import { getDictionary } from '@app/i18n';
+
+import { Locale } from '@shared/i18n';
+
+const NotFound = async ({ locale }: { locale: Locale }) => {
+  const dict = await getDictionary(locale);
+
+  return (
+    <PageGutter>
+      <MessageScreen title="404" description={dict.error.pageNotFound} />
+    </PageGutter>
+  );
+};
 
 export default NotFound;
