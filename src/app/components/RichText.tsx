4cabccaa793fbe31843e7e10286b5087a8cc00f5
Maksym Volkov <maksym.volkov@zapal.tech>
2024-08-16 18:14:30 +0000

    fix: richtext backgroundImage width full

--- src/app/components/RichText.tsx
+++ src/app/components/RichText.tsx
@@ -10,6 +10,7 @@ import { Locale } from '@shared/i18n';
 
 import { FeedbackForm } from './FeedbackForm';
 import { LexicalRenderer } from './LexicalRenderer';
+import { Media } from './Media';
 
 export const RichText: React.FC<
   Omit<React.HTMLAttributes<HTMLDivElement>, 'children'> & { children: any; invert?: boolean; locale?: Locale }
@@ -142,48 +143,49 @@ export const RichText: React.FC<
           },
           backgroundImage: ({ fields }: any = {}) => {
             return (
-              <div
-                className={clsx(
-                  'block h-max w-full bg-cover bg-center px-6 py-6 md:px-8 md:py-10',
-                  fields?.textColor === 'white' && 'text-gray-50',
-                )}
-                style={{
-                  backgroundImage: `url('${fields?.image?.url}')`,
-                }}
-              >
+              <div className={clsx('relative block h-max w-full', fields?.textColor === 'white' && 'text-gray-50')}>
+                <Media
+                  resource={fields?.image}
+                  imgClassName="bg-cover"
+                  className="absolute left-1/2 top-1/2 z-0 h-full w-dvw -translate-x-1/2 -translate-y-1/2
+                    overflow-hidden"
+                />
+
                 {fields?.richText && (
-                  <LexicalRenderer
-                    blocks={{
-                      columns: ({ fields }: any = {}) => {
-                        const columnsSizes = (fields.size as string).split(' ');
-                        const gridSize = columnsSizes.reduce((acc, size) => acc + parseInt(size), 0);
-                        const columnsToRender = columnsSizes.length;
+                  <div className="relative z-10 px-6 py-6 md:px-8 md:py-10">
+                    <LexicalRenderer
+                      blocks={{
+                        columns: ({ fields }: any = {}) => {
+                          const columnsSizes = (fields.size as string).split(' ');
+                          const gridSize = columnsSizes.reduce((acc, size) => acc + parseInt(size), 0);
+                          const columnsToRender = columnsSizes.length;
 
-                        const columns = Object.entries(fields as Record<string, any>)
-                          .filter(
-                            ([field]) =>
-                              field.startsWith(columnNamePrefix) &&
-                              parseInt(field[field.length - 1]) <= columnsToRender,
-                          )
-                          .map(([, value]) => value);
+                          const columns = Object.entries(fields as Record<string, any>)
+                            .filter(
+                              ([field]) =>
+                                field.startsWith(columnNamePrefix) &&
+                                parseInt(field[field.length - 1]) <= columnsToRender,
+                            )
+                            .map(([, value]) => value);
 
-                        return (
-                          <div className={`grid grid-cols-1 gap-x-5 gap-y-6 xl:grid-cols-${gridSize}`}>
-                            {columns.map((column, idx) => (
-                              <div
-                                key={`column-${idx}`}
-                                className={`xl:col-span-${columnsSizes[idx]} flex flex-col gap-6`}
-                              >
-                                <LexicalRenderer>{column}</LexicalRenderer>
-                              </div>
-                            ))}
-                          </div>
-                        );
-                      },
-                    }}
-                  >
-                    {fields?.richText}
-                  </LexicalRenderer>
+                          return (
+                            <div className={`grid grid-cols-1 gap-x-5 gap-y-6 xl:grid-cols-${gridSize}`}>
+                              {columns.map((column, idx) => (
+                                <div
+                                  key={`column-${idx}`}
+                                  className={`xl:col-span-${columnsSizes[idx]} flex flex-col gap-6`}
+                                >
+                                  <LexicalRenderer>{column}</LexicalRenderer>
+                                </div>
+                              ))}
+                            </div>
+                          );
+                        },
+                      }}
+                    >
+                      {fields?.richText}
+                    </LexicalRenderer>
+                  </div>
                 )}
               </div>
             );
