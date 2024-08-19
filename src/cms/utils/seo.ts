2a3cd766f9f9f322c71f2eec3d65245a6b2fb09a
Maksym Volkov <maksym.volkov@zapal.tech>
2024-08-19 18:08:06 +0000

    feat: cms seo

--- src/cms/utils/seo.ts
+++ src/cms/utils/seo.ts
@@ -1,14 +1,16 @@
 import { GenerateTitle } from '@payloadcms/plugin-seo/types';
 import { appName } from '@shared';
 
+import { Page } from '@cms/types/generated-types';
+
 import { Locale } from '@shared/i18n';
 
 const localeAppNames = (locale?: Locale) => (locale === Locale.Ukrainian ? 'Земляки Рєпіна' : appName);
 
-export const generateTitle: GenerateTitle = ({ doc, locale }) => {
+export const generateTitle: GenerateTitle<Page> = ({ doc, locale }) => {
   const suffix = localeAppNames(locale as Locale | undefined);
   const separator = ' | ';
-  const title = (doc as any)?.title?.value as string | undefined;
+  let title = 'title' in doc ? doc.title : null;
 
   if (typeof title === 'string' && title) return `${title}${separator}${suffix}`;
 
